a50935cfb9060b4b835bc3b35d881a200f37ef8f
Daniel <dnedrud1@gmail.com>
2014-11-09 13:23:55 -0600

    Finished Hangman

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -12,12 +12,26 @@ class Game
     eligible_words = @words.select { |i| i.length > 4 && i.length < 13 && i[0] == i[0].downcase}
     word = eligible_words[rand(eligible_words.length)].upcase.chomp
     wrong_guesses = 9
-    missed_letters = []
+    wrong_letters = []
     correct_letters = []
     
     puts "Welcome to hangman!"
-    puts "Guess a letter!"
-    puts word_display(word,correct_letters)
+    
+    until victory(word,correct_letters)
+      puts "Guess a letter! (#{wrong_guesses} guesses remaining)"
+      puts word_display(word,correct_letters) + "  Wrong Letters: #{wrong_letters.join(" ")}"
+      guess = clean_guess(gets.chomp.upcase,correct_letters,wrong_letters)
+      if word.split("").include?(guess)
+      	correct_letters.push(guess)
+    	else
+    		wrong_letters.push(guess)
+    		wrong_guesses -= 1
+  		end
+  		
+  		return puts "Sorry, you didn't guess the word in time! It was \"#{word}\"." if wrong_guesses < 1
+    end
+    
+    puts "You won! The word was \"#{word}\"!" if wrong_guesses > 0  
     
   end
   
@@ -35,7 +49,23 @@ class Game
     end
     display_arr.join(" ")
   end
-      
+  
+  def clean_guess(guess,right,wrong)
+  	until ("A".."Z").to_a.include?(guess) && !right.include?(guess) && !wrong.include?(guess)
+  		puts "Oops! Please enter a valid letter!"
+  	  guess = gets.chomp.upcase
+	  end
+	  guess
+  end
+	  
+  def victory(word,letters)
+    if word.split("").all? { |i| letters.include?(i) }
+    	true
+  	else
+  	  false
+	  end
+  end
+  
 end
 
 Game.new.start
